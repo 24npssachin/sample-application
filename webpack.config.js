dd7a7806b6f8beac226098bde80e1ffd4c1f3f15
sachin kumar <67408163+24npssachin@users.noreply.github.com>
2025-06-07 07:17:21 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,25 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/client/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),  // where the HTML + JS will go
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: '@sucrase/webpack-loader',
-          options: {
-            transforms: ['jsx']
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: `
-        <!DOCTYPE html>
-        <html>
-          <head><meta charset="utf-8"><title>React App</title></head>
-          <body><div id="root"></div></body>
-        </html>
-      `
-    })
-  ],
-  mode: 'production'
+	entry: './src/client/index.js',  // <-- adjust if different
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build') // <-- output path for Docker
+		publicPath: '/', // for React router or SPAs
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: '@sucrase/webpack-loader',
+					options: {
+						transforms: ['jsx']
+					}
+				}
+			}
+		]
+	},
+	mode: 'production' // or 'development' as needed
 };
